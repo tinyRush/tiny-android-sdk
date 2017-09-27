2fcd9feb06ba0858f15d652e586564abddb82518
Manh Nguyen Van <manhnv1208@gmail.com>
2017-09-27 21:58:41 +0700

    [ManhNV] - Add file tiny

---
+++ tiny-api/src/main/java/com/tiny/tiny_api/TinySDK.java
@@ -0,0 +1,14 @@
+package com.tiny.tiny_api;
+
+/********************************************************
+ * File Name : TinySDK.java
+ * Author : ManhNV
+ * Date : 2017-09-27
+ * Description: 
+ * Last-modified by : ManhNV
+ * Last-modified : 2017-09-27
+ ********************************************************/
+
+
+public class TinySDK {
+}
